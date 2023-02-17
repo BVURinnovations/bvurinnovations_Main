800a427165be31d968bedf85c21d8ca76e9319da
Vijay Rathore <vijay.rathore@aventior.com>
2023-02-17 16:31:44 +0530

    Initial commit

---
+++ src/main/java/com/bvurinnovations/BvurinnovationsApplication.java
@@ -0,0 +1,13 @@
+package com.bvurinnovations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvurinnovationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvurinnovationsApplication.class, args);
+	}
+
+}
